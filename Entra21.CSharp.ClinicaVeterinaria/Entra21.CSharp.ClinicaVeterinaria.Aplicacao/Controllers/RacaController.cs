a342a929d883aa0fd00bfc0bbeafce193b71b77c
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-03 22:09:58 -0300

    Adicionado estrutura do projeto web

---
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
+{
+    // Dois pontos Herança(mais para frente)
+    public class RacaController : Controller
+    {
+        /// <summary>
+        /// Endpoint que permite listar todas as raças
+        /// </summary>
+        /// <returns>Retorna a página html com as raças</returns>
+        [Route("/raca")]
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return View("Index");
+        }
+
+        [Route("/raca/cadastrar")]
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [Route("/raca/registrar")]
+        [HttpGet]
+        public IActionResult Registrar(
+            [FromQuery] string nome,
+            [FromQuery] string especie)
+        {
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
