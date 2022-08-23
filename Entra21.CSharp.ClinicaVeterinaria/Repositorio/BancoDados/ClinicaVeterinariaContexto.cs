1340e95518d5ec7f6e4703eb8e53ecac6f896e74
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-22 21:48:00 -0300

    Adicionado a opção de code first com migrations

--- Entra21.CSharp.ClinicaVeterinaria/Repositorio/BancoDados/ClinicaVeterinariaContexto.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/BancoDados/ClinicaVeterinariaContexto.cs
@@ -1,4 +1,5 @@
 ﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades;
+using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Mapeamentos;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados
@@ -15,9 +16,22 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            /*
+            * Documentação: https://docs.microsoft.com/pt-br/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
+            * Necessário instalar a ferramenta do dotnet ef core
+            *      dotnet tool install --global dotnet-ef
+            * 1ª etapa - Criar a entidade Raca.cs
+            * 2ª etapa - Criar o mapemanto da entidade para tabela RacaMapeamento.cs
+            * 3ª etapa - Registrar o mapeamento no próprio Contexto
+            * 4ª etapa - Gerar a migration
+            *      dotnet ef migrations add AdicionarRacaTabela --project Repositorio --startup-project Entra21.CSharp.ClinicaVeterinaria.Aplicacao
+            * 5ª etapa - A migration poderá ser aplicada de duas formas:
+            *   - executar comando para aplicar a migration sem a
+            *          necessidade de executar a aplicação
+            *          dotnet ef database update --project Repositorio --startup-project Entra21.CSharp.ClinicaVeterinaria.Aplicacao
+            *   - executar a aplicação irá aplicar a migration */
 
-            modelBuilder.Entity<Raca>().ToTable("racas");
+            modelBuilder.ApplyConfiguration(new RacaMapeamento());
         }
     }
 }
